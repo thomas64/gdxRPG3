04d6537d63bccc98300b710d7c07433f7dfdb873
Thomas M. Hesselink <mail@t64.nl>
2021-10-03 21:26:15 +0200

    removed all modules again. only core and desktop.

--- server/src/main/java/nl/t64/cot/server/ServerLauncher.java
+++
@@ -1,8 +0,0 @@
-package nl.t64.cot.server;
-
-/** Launches the server application. */
-public class ServerLauncher {
-	public static void main(String[] args) {
-		// TODO Implement server application.
-	}
-}
