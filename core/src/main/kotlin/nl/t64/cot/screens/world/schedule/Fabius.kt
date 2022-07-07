0843811cea30e1f53cf72d27bd6111674bc79bab
Thomas M. Hesselink <thomas.hesselink@alliander.com>
2022-07-08 00:24:11 +0200

    small test update fabius.

--- core/src/main/kotlin/nl/t64/cot/screens/world/schedule/Fabius.kt
+++ core/src/main/kotlin/nl/t64/cot/screens/world/schedule/Fabius.kt
@@ -19,8 +19,9 @@ class Fabius : Schedule() {
             // @formatter:off
             State("honeywood",      SOUTH,  IDLE,       7, 30,    7, 40,    550, 350,    550, 350),
             State("honeywood",      EAST,   IDLE,       7, 40,    7, 41,    550, 350,    550, 350),
-            State("honeywood",      EAST,   WALKING,    7, 41,    7, 45,    550, 350,    600, 350),
-            State("honeywood",      NORTH,  WALKING,    7, 45,    7, 50,    600, 350,    624, 484),
+            State("honeywood",      EAST,   WALKING,    7, 41,    7, 42,    550, 350,    700, 350),
+            State("honeywood",      WEST,   WALKING,    7, 42,    7, 43,    700, 350,    600, 350),
+            State("honeywood",      NORTH,  WALKING,    7, 43,    7, 50,    600, 350,    624, 484),
             State("honeywood_inn",  NORTH,  WALKING,    7, 50,    7, 53,    620,  40,    620, 250),
             State("honeywood_inn",  NORTH,  IDLE,       7, 53,    9, 55,    620, 250,    620, 250),
             // @formatter:on
