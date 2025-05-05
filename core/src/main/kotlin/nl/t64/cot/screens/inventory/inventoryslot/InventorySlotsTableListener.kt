c6d3302daff0cfbad1878afff404cce5fba510bf
Thomas M. Hesselink <internet@t64.nl>
2025-05-05 22:59:53 +0200

    able to hold key down in inventory.

--- core/src/main/kotlin/nl/t64/cot/screens/inventory/inventoryslot/InventorySlotsTableListener.kt
+++ core/src/main/kotlin/nl/t64/cot/screens/inventory/inventoryslot/InventorySlotsTableListener.kt
@@ -3,21 +3,55 @@ package nl.t64.cot.screens.inventory.inventoryslot
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
+import com.badlogic.gdx.utils.Timer
 
+
+private const val INITIAL_REPEAT_DELAY: Float = 0.4f
+private const val REPEAT_INTERVAL: Float = 0.08f
 
 class InventorySlotsTableListener(
     private val selectNewSlot: (Int) -> Unit,
-    private val slotsInRow: Int
+    private val slotsPerRow: Int
 ) : InputListener() {
 
+    private var repeatTask: Timer.Task? = null
+
     override fun keyDown(event: InputEvent, keycode: Int): Boolean {
-        when (keycode) {
-            Input.Keys.UP -> selectNewSlot.invoke(-slotsInRow)
-            Input.Keys.DOWN -> selectNewSlot.invoke(slotsInRow)
-            Input.Keys.LEFT -> selectNewSlot.invoke(-1)
-            Input.Keys.RIGHT -> selectNewSlot.invoke(1)
-        }
+        if (repeatTask != null) return true
+
+        val deltaSlotIndex: Int = getDeltaForKey(keycode) ?: return true
+        selectNewSlot.invoke(deltaSlotIndex)
+        startRepeatTask(deltaSlotIndex)
         return true
+    }
+
+    override fun keyUp(event: InputEvent, keycode: Int): Boolean {
+        stopRepeatTask()
+        return true
+    }
+
+    private fun getDeltaForKey(keycode: Int): Int? {
+        return when (keycode) {
+            Input.Keys.UP -> -slotsPerRow
+            Input.Keys.DOWN -> slotsPerRow
+            Input.Keys.LEFT -> -1
+            Input.Keys.RIGHT -> 1
+            else -> null
+        }
+    }
+
+    private fun startRepeatTask(deltaSlotIndex: Int) {
+        repeatTask = object : Timer.Task() {
+            override fun run() {
+                selectNewSlot.invoke(deltaSlotIndex)
+            }
+        }
+        Timer.schedule(repeatTask, INITIAL_REPEAT_DELAY, REPEAT_INTERVAL)
+    }
+
+    private fun stopRepeatTask() {
+        repeatTask?.cancel()
+        repeatTask = null
     }
 
 }
