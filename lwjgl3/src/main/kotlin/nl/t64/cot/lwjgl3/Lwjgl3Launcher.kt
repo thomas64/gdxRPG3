28d4d5f9d2940fe06cab3ba85c59baaf04384bf0
Thomas M. Hesselink <mail@t64.nl>
2026-02-27 16:50:42 +0100

    version upgrades. liftoff. gradle. libgdx. dependabot.

--- lwjgl3/src/main/kotlin/nl/t64/cot/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/nl/t64/cot/lwjgl3/Lwjgl3Launcher.kt
@@ -40,6 +40,17 @@ fun main() {
             //// They can also be loaded from the root of assets/ .
             setWindowIcon("sprites/icon.png", *(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
             setPreferencesConfig("T64.nl/", Files.FileType.External)
+
+            //// This could improve compatibility with Windows machines with buggy OpenGL drivers, Macs
+            //// with Apple Silicon that have to emulate compatibility with OpenGL anyway, and more.
+            //// This uses the dependency `com.badlogicgames.gdx:gdx-lwjgl3-angle` to function.
+            //// You would need to add this line to lwjgl3/build.gradle , below the dependency on `gdx-backend-lwjgl3`:
+            ////     implementation "com.badlogicgames.gdx:gdx-lwjgl3-angle:$gdxVersion"
+            //// You can choose to add the following line and the mentioned dependency if you want; they
+            //// are not intended for games that use GL30 (which is compatibility with OpenGL ES 3.0).
+            //// Know that it might not work well in some cases.
+//        setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.ANGLE_GLES20, 0, 0)
+
         }
     )
 
--- lwjgl3/src/main/kotlin/nl/t64/cot/lwjgl3/StartupHelper.kt
+++ lwjgl3/src/main/kotlin/nl/t64/cot/lwjgl3/StartupHelper.kt
@@ -12,174 +12,162 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//Note, the above license and copyright applies to this file only.
+// Note, the above license and copyright applies to this file only.
 package nl.t64.cot.lwjgl3
 
 import com.badlogic.gdx.Version
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3NativesLoader
 import org.lwjgl.system.JNI
+import org.lwjgl.system.linux.UNISTD
 import org.lwjgl.system.macosx.LibC
 import org.lwjgl.system.macosx.ObjCRuntime
-import java.io.BufferedReader
 import java.io.File
-import java.io.InputStreamReader
 import java.lang.management.ManagementFactory
-import java.util.*
 
 
 /**
- * Adds some utilities to ensure that the JVM was started with the
- * `-XstartOnFirstThread` argument, which is required on macOS for LWJGL 3
- * to function. Also helps on Windows when users have names with characters from
- * outside the Latin alphabet, a common cause of startup crashes.
+ * A helper object for game startup, featuring three utilities related to LWJGL3 on various operating systems.
+ *
+ * The utilities are as follows:
+ * - Windows: Prevents a common crash related to LWJGL3's extraction of shared library files.
+ * - macOS: Spawns a child JVM process with `-XstartOnFirstThread` in the JVM args (if it was not already). This is required for LWJGL3 to work on macOS.
+ * - Linux (NVIDIA GPUs only): Spawns a child JVM process with the `__GL_THREADED_OPTIMIZATIONS` [Environment Variable][System.getenv] set to `0` (if it was not already). This is required for LWJGL3 to work on Linux with NVIDIA GPUs.
  *
  * [Based on this java-gaming.org post by kappa](https://jvm-gaming.org/t/starting-jvm-on-mac-with-xstartonfirstthread-programmatically/57547)
  * @author damios
  */
-class StartupHelper private constructor() {
+object StartupHelper {
 
-    init {
-        throw UnsupportedOperationException()
+    private const val JVM_RESTARTED_ARG = "jvmIsRestarted"
+
+    /**
+     * Must only be called on Linux. Check OS first (or use short-circuit evaluation)!
+     * @return whether NVIDIA drivers are present on Linux.
+     */
+    fun isLinuxNvidia(): Boolean =
+        File("/proc/driver").list { _, path: String -> "NVIDIA" in path.uppercase() }.isNullOrEmpty().not()
+
+    /**
+     * Applies the utilities as described by [StartupHelper]'s KDoc.
+     *
+     * All [Environment Variables][System.getenv] are copied to the child JVM process (if it is spawned), as specified by [ProcessBuilder.environment]; the same applies for [System Properties][System.getProperties].
+     *
+     * **Usage:**
+     *
+     * ```
+     * fun main() {
+     *   if (StartupHelper.startNewJvmIfRequired()) return
+     *   // ...
+     * }
+     * ```
+     * @param inheritIO whether I/O should be inherited in the child JVM process. Please note that enabling this will block the thread until the child JVM process stops executing.
+     * @return whether a child JVM process was spawned or not.
+     */
+    @JvmOverloads
+    fun startNewJvmIfRequired(inheritIO: Boolean = true): Boolean {
+        val osName: String = System.getProperty("os.name").lowercase()
+        if ("mac" in osName) return startNewJvm0(isMac = true, inheritIO)
+        if ("windows" in osName) {
+            // Here, we are trying to work around an issue with how LWJGL3 loads its extracted .dll files.
+            // By default, LWJGL3 extracts to the directory specified by "java.io.tmpdir": usually, the user's home.
+            // If the user's name has non-ASCII (or some non-alphanumeric) characters in it, that would fail.
+            // By extracting to the relevant "ProgramData" folder, which is usually "C:\ProgramData", we avoid this.
+            // We also temporarily change the "user.name" property to one without any chars that would be invalid.
+            // We revert our changes immediately after loading LWJGL3 natives.
+            val programData: String = System.getenv("ProgramData") ?: "C:\\Temp"
+            val prevTmpDir: String = System.getProperty("java.io.tmpdir", programData)
+            val prevUser: String = System.getProperty("user.name", "libGDX_User")
+            System.setProperty("java.io.tmpdir", "$programData\\libGDX-temp")
+            System.setProperty("user.name", "User_${prevUser.hashCode()}_GDX${Version.VERSION}".replace('.', '_'))
+            Lwjgl3NativesLoader.load()
+            System.setProperty("java.io.tmpdir", prevTmpDir)
+            System.setProperty("user.name", prevUser)
+            return false
+        }
+        return startNewJvm0(isMac = false, inheritIO)
     }
 
-    companion object {
+    private const val MAC_JRE_ERR_MSG: String =
+        "A Java installation could not be found. If you are distributing this app with a bundled JRE, be sure to set the '-XstartOnFirstThread' argument manually!"
+    private const val LINUX_JRE_ERR_MSG: String =
+        "A Java installation could not be found. If you are distributing this app with a bundled JRE, be sure to set the environment variable '__GL_THREADED_OPTIMIZATIONS' to '0'!"
+    private const val CHILD_LOOP_ERR_MSG: String =
+        "The current JVM process is a spawned child JVM process, but StartupHelper has attempted to spawn another child JVM process!  This is a broken state, and should not normally happen!  Your game may crash or not function properly!"
 
-        private const val JVM_RESTARTED_ARG = "jvmIsRestarted"
-
-        /**
-         * Starts a new JVM if the application was started on macOS without the
-         * `-XstartOnFirstThread` argument. This also includes some code for
-         * Windows, for the case where the user's home directory includes certain
-         * non-Latin-alphabet characters (without this code, most LWJGL3 apps fail
-         * immediately for those users). Returns whether a new JVM was started and
-         * thus no code should be executed.
-         *
-         * **Usage:**
-         *
-         * ```
-         * fun main() {
-         *   if (StartupHelper.startNewJvmIfRequired(true)) return // This handles macOS support and helps on Windows.
-         *   // after this is the actual main method code
-         * }
-         * ```
-         *
-         * @param redirectOutput
-         * whether the output of the new JVM should be rerouted to the
-         * old JVM, so it can be accessed in the same place; keeps the
-         * old JVM running if enabled
-         * @return whether a new JVM was started and thus no code should be executed
-         * in this one
-         */
-        @JvmOverloads
-        fun startNewJvmIfRequired(redirectOutput: Boolean = true): Boolean {
-            val osName = System.getProperty("os.name").lowercase(Locale.getDefault())
-            if (!osName.contains("mac")) {
-                if (osName.contains("windows")) {
-                    // Here, we are trying to work around an issue with how LWJGL3 loads its extracted .dll files.
-                    // By default, LWJGL3 extracts to the directory specified by "java.io.tmpdir", which is usually the user's home.
-                    // If the user's name has non-ASCII (or some non-alphanumeric) characters in it, that would fail.
-                    // By extracting to the relevant "ProgramData" folder, which is usually "C:\ProgramData", we avoid this.
-                    // We also temporarily change the "user.name" property to one without any chars that would be invalid.
-                    // We revert our changes immediately after loading LWJGL3 natives.
-                    val programData = System.getenv("ProgramData") ?: "C:\\Temp\\"
-                    val prevTmpDir = System.getProperty("java.io.tmpdir", programData)
-                    val prevUser = System.getProperty("user.name", "libGDX_User")
-                    System.setProperty("java.io.tmpdir", "$programData/libGDX-temp")
-                    System.setProperty("user.name", "User_${prevUser.hashCode()}_GDX${Version.VERSION}".replace('.', '_'))
-                    Lwjgl3NativesLoader.load()
-                    System.setProperty("java.io.tmpdir", prevTmpDir)
-                    System.setProperty("user.name", prevUser)
-                }
-                return false
-            }
-
+    /**
+     * Spawns a child JVM process if on macOS or NVIDIA Linux.
+     *
+     * All [Environment Variables][System.getenv] are copied to the child JVM process (if it is spawned), as specified by [ProcessBuilder.environment]; the same applies for [System Properties][System.getProperties].
+     *
+     * @param isMac whether the current OS is macOS. If this is `false` then the current OS is assumed to be Linux (and an immediate check for NVIDIA drivers is performed).
+     * @param inheritIO whether I/O should be inherited in the child JVM process. Please note that enabling this will block the thread until the child JVM process stops executing.
+     * @return whether a child JVM process was spawned or not.
+     */
+    fun startNewJvm0(isMac: Boolean, inheritIO: Boolean): Boolean {
+        val processID: Long = if (isMac) LibC.getpid() else UNISTD.getpid().toLong()
+        if (!isMac) {
+            // No need to restart non-NVIDIA Linux
+            if (!isLinuxNvidia()) return false
+            // check whether __GL_THREADED_OPTIMIZATIONS is already disabled
+            if (System.getenv("__GL_THREADED_OPTIMIZATIONS") == "0") return false
+        } else {
             // There is no need for -XstartOnFirstThread on Graal native image
-            if (System.getProperty("org.graalvm.nativeimage.imagecode", "").isNotEmpty()) {
-                return false
-            }
+            if (System.getProperty("org.graalvm.nativeimage.imagecode", "").isNotEmpty()) return false
 
             // Checks if we are already on the main thread, such as from running via Construo.
-            val objcMsgSend = ObjCRuntime.getLibrary().getFunctionAddress("objc_msgSend")
-            val nsThread = ObjCRuntime.objc_getClass("NSThread")
-            val currentThread = JNI.invokePPP(nsThread, ObjCRuntime.sel_getUid("currentThread"), objcMsgSend)
-            val isMainThread = JNI.invokePPZ(currentThread, ObjCRuntime.sel_getUid("isMainThread"), objcMsgSend)
+            val objcMsgSend: Long = ObjCRuntime.getLibrary().getFunctionAddress("objc_msgSend")
+            val nsThread: Long = ObjCRuntime.objc_getClass("NSThread")
+            val currentThread: Long = JNI.invokePPP(nsThread, ObjCRuntime.sel_getUid("currentThread"), objcMsgSend)
+            val isMainThread: Boolean = JNI.invokePPZ(currentThread, ObjCRuntime.sel_getUid("isMainThread"), objcMsgSend)
             if (isMainThread) return false
 
-            val pid = LibC.getpid()
-
-            // check whether -XstartOnFirstThread is enabled
-            if ("1" == System.getenv("JAVA_STARTED_ON_FIRST_THREAD_$pid")) {
-                return false
-            }
-
-            // check whether the JVM was previously restarted
-            // avoids looping, but most certainly leads to a crash
-            if ("true" == System.getProperty(JVM_RESTARTED_ARG)) {
-                System.err.println(
-                    "There was a problem evaluating whether the JVM was started with the -XstartOnFirstThread argument."
-                )
-                return false
-            }
-
-            // Restart the JVM with -XstartOnFirstThread
-            val jvmArgs = ArrayList<String?>()
-            val separator = System.getProperty("file.separator", "/")
-            // The following line is used assuming you target Java 8, the minimum for LWJGL3.
-            val javaExecPath = System.getProperty("java.home") + separator + "bin" + separator + "java"
-
-            // If targeting Java 9 or higher, you could use the following instead of the above line:
-            //String javaExecPath = ProcessHandle.current().info().command().orElseThrow();
-            if (!File(javaExecPath).exists()) {
-                System.err.println(
-                    "A Java installation could not be found. If you are distributing this app with a bundled JRE, be sure to set the -XstartOnFirstThread argument manually!"
-                )
-                return false
-            }
-
-            jvmArgs.add(javaExecPath)
-            jvmArgs.add("-XstartOnFirstThread")
-            jvmArgs.add("-D$JVM_RESTARTED_ARG=true")
-            jvmArgs.addAll(ManagementFactory.getRuntimeMXBean().inputArguments)
-            jvmArgs.add("-cp")
-            jvmArgs.add(System.getProperty("java.class.path"))
-            var mainClass = System.getenv("JAVA_MAIN_CLASS_$pid")
-            if (mainClass == null) {
-                val trace = Thread.currentThread().stackTrace
-                mainClass = if (trace.isNotEmpty()) {
-                    trace[trace.size - 1].className
-                } else {
-                    System.err.println("The main class could not be determined.")
-                    return false
-                }
-            }
-            jvmArgs.add(mainClass)
-
-            try {
-                if (!redirectOutput) {
-                    val processBuilder = ProcessBuilder(jvmArgs)
-                    processBuilder.start()
-                } else {
-                    val process = ProcessBuilder(jvmArgs)
-                        .redirectErrorStream(true).start()
-                    val processOutput = BufferedReader(
-                        InputStreamReader(process.inputStream)
-                    )
-                    var line: String?
-
-                    while (processOutput.readLine().also { line = it } != null) {
-                        println(line)
-                    }
-
-                    process.waitFor()
-                }
-            } catch (e: Exception) {
-                System.err.println("There was a problem restarting the JVM")
-                e.printStackTrace()
-            }
-
-            return true
+            if (System.getenv("JAVA_STARTED_ON_FIRST_THREAD_$processID") == "1") return false
         }
 
-    }
+        // Check whether this JVM process is a child JVM process already.
+        // This state shouldn't (usually) be reachable, but this stops us from endlessly spawning new child JVM processes.
+        if (System.getProperty(JVM_RESTARTED_ARG) == "true") {
+            System.err.println(CHILD_LOOP_ERR_MSG)
+            return false
+        }
 
+        // Spawn the child JVM process with updated environment variables or JVM args
+        val jvmArgs: MutableList<String> = mutableListOf()
+        // The following line is used assuming you target Java 8, the minimum for LWJGL3.
+        val javaExecPath = "${System.getProperty("java.home")}/bin/java"
+        // If targeting Java 9 or higher, you could use the following instead of the above line:
+        //val javaExecPath = ProcessHandle.current().info().command().orElseThrow()
+        if (!File(javaExecPath).exists()) {
+            System.err.println(if (isMac) MAC_JRE_ERR_MSG else LINUX_JRE_ERR_MSG)
+            return false
+        }
+
+        jvmArgs += javaExecPath
+        if (isMac) jvmArgs += "-XstartOnFirstThread"
+        jvmArgs += "-D$JVM_RESTARTED_ARG=true"
+        jvmArgs += ManagementFactory.getRuntimeMXBean().inputArguments
+        jvmArgs += "-cp"
+        jvmArgs += System.getProperty("java.class.path")
+        jvmArgs += System.getenv("JAVA_MAIN_CLASS_$processID") ?: run {
+            val trace: Array<StackTraceElement> = Thread.currentThread().stackTrace
+            if (trace.isNotEmpty()) return@run trace[trace.lastIndex].className
+            else {
+                System.err.println("The main class could not be determined through stacktrace.")
+                return false
+            }
+        }
+
+        try {
+            val processBuilder = ProcessBuilder(jvmArgs)
+            if (!isMac) processBuilder.environment()["__GL_THREADED_OPTIMIZATIONS"] = "0"
+
+            if (!inheritIO) processBuilder.start()
+            else processBuilder.inheritIO().start().waitFor()
+        } catch (e: Exception) {
+            System.err.println("There was a problem restarting the JVM.")
+            e.printStackTrace()
+        }
+
+        return true
+    }
 }
