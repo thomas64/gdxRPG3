2f8871a7acefe8b9801752674fc9f1907a86060f
Thomas M. Hesselink <internet@t64.nl>
2026-08-19 02:38:52 +0200

    setup forest trail map.

--- assets/maps/tilesets/fsm_forest01_b.tsx
+++ assets/maps/tilesets/fsm_forest01_b.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="fsm_forest01_b" tilewidth="24" tileheight="24" tilecount="1024" columns="32">
+<tileset version="1.11" tiledversion="1.12.2" name="fsm_forest01_b" tilewidth="24" tileheight="24" tilecount="1024" columns="32">
  <image source="fsm_forest01_b.png" width="768" height="768"/>
 </tileset>
